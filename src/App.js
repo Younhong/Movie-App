6cda15f5f3a57a67fbf4b8946a4728db43936855
younhong <younhong@kakao.com>
2020-11-03 15:35:24 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,37 @@ import React from 'react';
 // import React, {Component} from 'react';
 // => can extend Component instead of React.Component
 import PropTypes from 'prop-types';
-import FoodApp from './food';
 
 class App extends React.Component{
   state = {
     count: 0
-  }
+  };
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }));
+  };
+  minus = () => {
+    if (this.state.count === 0) {
+      this.setState(current => ({
+        count: 0
+      }));
+    } else {
+      this.setState(current => ({
+        count: current.count - 1
+      }));      
+    }
+  };
+  // changing state will re-render every time
   render(){
-    return <h1>I am who I am {this.state.count}</h1>
+    return (
+      <div>
+        <h1>I am who I am</h1>
+        <h1>Start Counting {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
 }
 
